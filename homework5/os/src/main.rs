4e692490ca1becdd0b2616c6ff0068098d7d8e76
WangGUGU <2272323106@qq.com>
2021-04-29 11:27:39 +0800

    页表

---
+++ homework5/os/src/main.rs
@@ -0,0 +1,50 @@
+//! - #![no-std] 
+//!   禁用标准库
+#![no_std]
+//! - #![no_main]
+//!   不使用 main 函数作为程序入口
+#![no_main]
+//! 使用汇编
+//! - #![feature(llvm_asm)]
+//!   内嵌汇编
+#![feature(llvm_asm)]
+
+//! - #![feature(global_asm)]
+//!   内嵌汇编文件
+#![feature(global_asm)]
+
+//! - #![feature(panic_info_message)] 
+//!   panic! 时，获取其中的信息并打印
+#![feature(panic_info_message)]
+
+//! - #![feature(alloc_error_handler)]
+//!   内存分配错误回调
+#![feature(alloc_error_handler)]
+
+// 汇编程序入口
+global_asm!(include_str!("entry.asm"));
+
+#[macro_use]
+mod console;
+mod panic;
+mod sbi;
+mod interrupt;
+mod memory;
+extern crate alloc;
+
+/// Rust 的⼊⼝函数
+///
+/// 在 `_start` 为我们进⾏了⼀系列准备之后，这是第⼀个被调⽤的 Rust 函数
+#[no_mangle]
+pub extern "C" fn rust_main() -> ! {
+	// 初始化各种模块
+	interrupt::init();
+	memory::init();
+
+	let remap = memory::mapping::MemorySet::new_kernel().unwrap();
+	remap.activate();
+
+	println!("kernel remapped");
+
+	panic!()
+}
